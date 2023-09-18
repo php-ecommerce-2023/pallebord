71400fc06f885736055e2bc95a345ad736953975
Mathias Ritter <huskmigffs@gmail.com>
2023-09-18 11:36:48 +0100

    ny

---
+++ resources/views/10192762.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Pallebord product</title>
+    <link rel="stylesheet" href="../../css/mathias.css">
+</head>
+<body>
+     
+  
+<div class="d-flex min-vh-100">
+	<div class="container align-self-center">
+
+		<div class="row justify-content-center">
+
+			<div class="col-12 col-lg-8 col-xl-6 text-center">
+
+				<div class="lc-block mb-2">
+                <img class="img-fluid" w-75 src="https://images.unsplash.com/photo-1462286785289-59aa87d355a3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80" alt="Mathias Ritter">
+					</div>
+				<div class="lc-block mb-4">
+					<div editable="rich">
+
+						<h2>Køb en palle</h2>
+
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a lacus est.
+							Test diam metus, lobortis non augue at, placerat viverra risus. Cras ornare
+							faucibus laoreet. Aenean vel nisi in ipsum congue fermentum et ut arcu. Proin leo
+							diam,
+							vulputate eu tellus ac, mattis cursus nunc.</p>
+					</div>
+				</div>
+				<form class="lc-block" method="POST" action="/buy">
+                 <button type="submit" class="btn btn-primary" role="button">Buy a pallet now!</button>
+                </form>
+			</div>
+		</div>
+	</div>
+
+
+
+</div> 
+</body>
+</html>
