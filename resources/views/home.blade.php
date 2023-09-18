88395bb93d4076c1a054c36b9ae75a6e01808088
Benjamin Jon Hansen <ben.jon.han@gmail.com>
2023-09-18 13:07:08 +0200

    oprettelse af forside

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        * {
+    box-sizing: border-box;
+            }
+
+    header {
+        display: flex;
+        
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <header>
+            <section class='logo'>
+                <h1>Pallebord.dk</h1>
+            </section>
+            <section class = "nav-menu">
+                <a href='#'>Shop</a>
+                <a href='#'>Om os</a>
+                <a href='#'>Kontakt</a>
+            </section>
+        
+        </header>
+
+
+
+    </main>
+
+</body>
+</html>
