99a3e1f52c3146246bcac14dde077969abf6ec83
Sima Kocis <skinner.kocis@gmail.com>
2023-09-18 13:44:12 +0200

    my first commit

---
+++ resources/views/sima.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body{
+        display: inline;
+    }
+</style>
+<body>
+
+<h1>TEST</h1>
+
+<form action="/buy" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit"> SUBMIT </input>
+</form>
+    
+</body>
+</html>
