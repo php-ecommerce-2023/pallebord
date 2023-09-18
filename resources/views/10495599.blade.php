66ee07d46a4aaff4601e86af50d806a5970cbb4c
stfn <steffenriis@outlook.com>
2023-09-18 12:47:33 +0200

    Added view for 10495599

---
+++ resources/views/10495599.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bord min palle</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body style="background-color: #333333">
+
+
+
+    <div class="overflow-hidden">
+        <div class="container-fluid col-xxl-8">
+            <div class="row flex-lg-nowrap align-items-center g-5">
+                <div class="order-lg-1 w-100">
+                <img style="clip-path: polygon(25% 0%, 100% 0%, 100% 99%, 0% 100%);" src="https://vcdn.polarismedia.no/735042f3-e86c-4869-8709-e36c0ccce021?fit=crop&h=600&q=80&tight=false&w=1000" class="d-block mx-lg-auto img-fluid" alt="Photo by Milad Fakurian" loading="lazy" width="2160" height="768">
+
+
+
+                </div>
+                <div class="col-lg-6 col-xl-5 text-center text-lg-start pt-lg-5 mt-xl-4">
+                    <div class="lc-block mb-4">
+                        <div editable="rich">
+                            <h1 class="fw-bold display-3" style="color: #ffffff">Pallebord</h1>
+                        </div>
+                    </div>
+
+                    <div class="lc-block mb-5">
+                        <div editable="rich">
+                            <p class="rfs-8" style="color: #ffffff"> Bordet på en FUCKING palle.</p>
+                        </div>
+                    </div>
+
+                    <form action="/buy" method="post">
+                        <button type="submit" class="btn btn-primary px-4 me-md-2 btn-lg">Køb PALLEBORD nu</button>
+                    </form>
+
+
+                </div>
+
+            </div><!-- /lc-block -->
+        </div>
+    </div>
+
+
+
+   
+
+</body>
+</html>
